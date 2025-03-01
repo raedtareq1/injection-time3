4cf0981993ac6c96e0833725661f843c118d7a1a
raedtareq1 <h.t.y.a.2001@gmail.com>
2025-03-01 16:43:41 +0200

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// Plugin Owl Slider
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        rtl: true,
+        loop: true,
+        margin: 10,
+        nav: true,
+        items: 1,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+    })
+    
+})
